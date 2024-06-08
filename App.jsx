77ddbb1be79f2b7fc99ae44bf0af16ecfb76db45
JavaRip <up941226@myport.ac.uk>
2024-06-08 14:26:05 +0100

    second page

--- App.jsx
+++ App.jsx
@@ -1,9 +1,12 @@
 import { ThemeProvider } from '@emotion/react';
-import { Button, MobileStepper, Paper, Typography, Stack, Card, Box } from '@mui/material';
+import { Typography, Stack, Card, Box } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import EmailIcon from '@mui/icons-material/Email';
 import StarRateIcon from '@mui/icons-material/StarRate';
+import LanguageIcon from '@mui/icons-material/Language';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import { useState, useEffect, useRef } from 'react';
 import { Star } from '@mui/icons-material';
 
@@ -28,20 +31,10 @@ const theme = createTheme({
             fontWeight: 400,
         }
     }
+
 })
 
 export default function App() {
-    const [activeStep, setActiveStep] = useState(0);
-    const intervalRef = useRef(null); // Using useRef to store the interval ID
-
-    // Clears the interval
-    const clearAutoScroll = () => {
-        if (intervalRef.current) {
-            clearInterval(intervalRef.current);
-            intervalRef.current = null;
-        }
-    };
-
     return (
         <ThemeProvider theme={theme}>
             <Box sx={{ background: 'skyblue', height: '100dvh', width: '100dvw' }}>
@@ -59,10 +52,10 @@ export default function App() {
                                 md: 'row',
                             }
                         }}
-
                     >
                         <Stack
-                            margin={4}
+                            mt={4}
+                            mb={2}
                             alignContent='center'
                             sx={{
                                 width: {
@@ -81,11 +74,10 @@ export default function App() {
                                         width: '100%',
                                         maxWidth: '40rem',
                                         p: 2,
-                                        width: '100%',
                                     }}
                                 >
-                                    <Typography variant='h5' mt={1} textAlign='center'>
-                                        Top rated outboard motor dealer
+                                    <Typography variant='h5' mb={1} mt={1} textAlign='center'>
+                                        Top rated outboard motor deals and repairs
                                     </Typography>
                                     <Stack justifyContent='center' direction='row'>
                                         <StarRateIcon />
@@ -97,25 +89,49 @@ export default function App() {
                                 </Box>
                             </Box>
 
-                            <Box margin='2rem auto'>
-                                <Stack mb={1} gap={2} direction='row'>
+                            <Box margin="0 auto" mt={2}>
+                                <Stack mb={1} gap={2} direction="row">
                                     <LocalPhoneIcon />
-                                    <Typography variant='h3'>
+                                    <Typography
+                                        variant="h3"
+                                        sx={{
+                                            fontSize: { xs: '1.0rem', sm: '1.5rem' }
+                                        }}
+                                    >
                                         (+44) 0712345678
                                     </Typography>
                                 </Stack>
-                                <Stack mb={1} gap={2} direction='row'>
+                                <Stack mb={1} gap={2} direction="row">
                                     <EmailIcon />
-                                    <Typography variant='h3'>
-                                        xavswartz@placeholder.com
+                                    <Typography
+                                        variant="h3"
+                                        sx={{
+                                            fontSize: { xs: '1.0rem', sm: '1.5rem' }
+                                        }}
+                                    >
+                                        info@outboardmotorsltd.com
+                                    </Typography>
+                                </Stack>
+                                <Stack mb={1} gap={2} direction="row">
+                                    <LanguageIcon />
+                                    <Typography
+                                        variant="h3"
+                                        sx={{
+                                            fontSize: { xs: '1.0rem', sm: '1.5rem' }
+                                        }}
+                                    >
+                                        <a href='https://linktr.ee/outboardmotorsltd'>
+                                            Linktree
+                                        </a>
                                     </Typography>
                                 </Stack>
                             </Box>
+
                         </Stack>
 
                         <Card
-                            raised={true}
                             sx={{
+                                boxShadow: 'none',
                                 width: {
                                     xs: '100%',
                                     sm: '100%',
@@ -125,22 +141,171 @@ export default function App() {
                                     sm: '1rem',
                                     md: '4rem',
                                 },
+                                background: 'skyblue',
                                 alignSelf: 'flex-end',
                                 height: '80%',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
                             }}
-
                         >
-
-                            <img src='/engine.png' alt='placeholder' />
+                            <img src='/outboard_engine.jpg' alt='placeholder' />
 
                         </Card>
                     </Stack>
                 </Box>
             </Box>
-            <Box sx={{ background: 'lightgray', height: '100dvh'}}>
-                <Stack>
-                    <Box></Box>
-                    <Card></Card>
+            <Box sx={{ background: 'lightgray', height: '100dvh' }}>
+                <Stack
+                    direction={{
+                        sm: 'column',
+                        md: 'row',
+                    }}
+                    sx={{
+                        height: '100%',
+                        width: '100%',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        gap: 4,
+                    }}
+                    pb={4}
+                >
+                    <Box
+                        outline='2px solid darkgray'
+                        height={340}
+                        minHeight={340}
+                        width={340}
+                        minWidth={340}
+                        borderRadius='100%'
+                        overflow='hidden'
+                        mt={4}
+                        ml={4}
+                        mr={4}
+                        sx={{
+                            display: 'flex',
+                            alignContent: 'center',
+                        }}
+                    >
+
+                        <img src='/xav_in_boat.jpg' alt='placeholder' />
+                    </Box>
+                    <Box
+                        height={340}
+                        width={340}
+                        sx={{
+                            width: '100%',
+                            maxWidth: '40rem',
+                            p: 2,
+                            mt: {
+                                sm: 0,
+                                // md: 20,
+                            }
+                        }}
+                    >
+                        <Stack
+                            justifyContent='flex-start'
+                            width='max-content'
+                            height={125}
+                            sx={{
+                                margin: {
+                                    xs: '0 auto',
+                                    sm: '0 auto',
+                                    md: '0',
+                                }
+                            }}
+                        >
+                            <Stack mb={1} gap={2} direction='row'>
+                                <FacebookIcon />
+                                <Typography
+                                    variant="h3"
+                                    sx={{
+                                        fontSize: '1.5rem',
+                                    }}
+                                >
+                                    <a
+                                        href='https://www.facebook.com/people/Outboard-Motors-Ltd/61560857936537/'
+                                    >Facebook</a>
+                                </Typography>
+                            </Stack>
+                            <Stack mb={1} gap={2} direction="row">
+                                <InstagramIcon />
+                                <Typography
+                                    variant="h3"
+                                    sx={{
+                                        fontSize: '1.5rem',
+                                    }}
+                                >
+                                    <a
+                                        href='https://www.instagram.com/outboard_motors_ltd?igsh=dTh1Y3gybG81ZGVl&utm_source=qr'
+                                    >Instagram</a>
+                                </Typography>
+                            </Stack>
+                            <Stack mb={1} gap={2} direction="row">
+                                <svg width={25}>
+                                    <path d="m13.511 5.853 4.005-4.117 2.325 2.381-4.201 4.005h5.909v3.305h-5.937l4.229 4.108-2.325 2.334-5.741-5.769-5.741 5.769-2.325-2.325 4.229-4.108H2V8.122h5.909L3.708 4.117l2.325-2.381 4.005 4.117V0h3.473v5.853zM10.038 16.16h3.473v7.842h-3.473V16.16z"></path>
+                                </svg>
+                                <Typography
+                                    variant="h3"
+                                    sx={{
+                                        fontSize: '1.5rem',
+                                    }}
+                                >
+                                    <a
+                                        href='https://linktr.ee/outboardmotorsltd'
+                                    >Linktree</a>
+                                </Typography>
+                            </Stack>
+                        </Stack>
+
+                        <Box
+                            width={500}
+                            margin='0 auto'
+                            sx={{
+                                margin: {
+                                    sm: '0 auto',
+                                    md: '0',
+                                }
+                            }}
+                        >
+                            <Typography
+                                variant='body1'
+                                margin='0 auto'
+                                mb={2}
+                            >
+                                Xavier is a skilled marine mechanic specialising in repair and
+                                maintaining two-stroke outboard engines. Dedicated to his craft
+                                ensuring every boat worked on runs smoothly and reliably.
+                            </Typography>
+
+                            <Typography variant='body1'>
+                                • Servicing
+                            </Typography>
+
+                            <Typography variant='body1'>
+                                • No spark diagnostic
+                            </Typography>
+
+                            <Typography variant='body1'>
+                                • Low compression
+                            </Typography>
+
+                            <Typography variant='body1'>
+                                • Rebuilds
+                            </Typography>
+
+                            <Typography variant='body1'>
+                                • Outboard Sales
+                            </Typography>
+
+                            <Typography variant='body1'>
+                                • No start fix
+                            </Typography>
+
+                            <Typography variant='body1'>
+                                • Health check
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Stack>
             </Box>
         </ThemeProvider>
